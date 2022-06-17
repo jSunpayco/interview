822dba7739ce0b94f05afba615442fc0be3c018d
Julian <julian_sunpacyo@dlsu.edu.ph>
2022-06-17 11:50:47 +0800

    Uploaded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <div class='container'>
+          <div class='child1'>
+            <h2>Schedule a Demo</h2>
+            <p>Learn More About FloQast</p>
+          </div>
+          <div class='child2'>
+            <div className="mygrid">
+              <div className="item">
+                <h3>Learn How Floqast Can </h3> <h3 className='improve'> Improve Your Month-End</h3>
+              </div>
+              <div className="item">
+                <input type="text" placeholder="First Name*"/> <input type="text" placeholder="Email*"/> <button>Schedule Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
